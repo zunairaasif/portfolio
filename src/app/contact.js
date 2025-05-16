05f14bd875e773eb7d10b756ef4c794527dc2ac6
Zunaira Asif <zunairaa.dev@gmail.com>
2025-05-16 16:25:56 +0500

    testing spline issue

--- src/app/contact.js
+++ src/app/contact.js
@@ -4,7 +4,7 @@ import Spline from "@splinetool/react-spline/next";
 const Contact = () => {
   return (
     <div id="contact" className="grid lg:grid-cols-2 grid-cols-1 lg:pb-0 pb-10">
-      <div className="xl:h-[600px] h-[500px] overflow-hidden">
+      <div className="md:block hidden xl:h-[600px] h-[500px] overflow-hidden">
         <div className="xl:h-[660px] h-[560px]">
           <Spline scene="https://prod.spline.design/X1HWOK5-C9qAhYfl/scene.splinecode" />
         </div>
